ad8dffd0ad1537823d56bde5db53919022ff76e0
Asif Zaman <eramasif16@gmail.com>
2023-04-22 03:40:08 +0600

    server initialization done

--- Node_js_modules.js
+++
@@ -1,7 +0,0 @@
-let http= require(`http`);
-
-http.createServer(function(req,res)
-{
- res.write("Hello from server.");
- res.end(); // end the response from server
-}).listen(6000);
---
+++ server creation/server_init.js
@@ -0,0 +1,18 @@
+// server initialization
+
+const http = require(`http`);
+
+let requestListener = (req, res) =>
+{
+res.writeHead(200);
+res.end("Server is responding");
+}; 
+
+//port initializing 
+var port=8080;
+var server=http.createServer(requestListener);
+console.log("The server is listening to port : "+port);
+server.listen(port);
+
+
+// server is ready to listen 
